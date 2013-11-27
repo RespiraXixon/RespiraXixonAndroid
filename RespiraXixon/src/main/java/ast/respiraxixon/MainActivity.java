282e523d40f53b6be7b3dfc58ba2e4758ba5eaf1
RespiraXixon <nahum.ballina@gmail.com>
2013-11-27 01:01:46 +0100

    Commit Inicial

---
+++ RespiraXixon/src/main/java/ast/respiraxixon/MainActivity.java
@@ -0,0 +1,77 @@
+package ast.respiraxixon;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import java.util.Locale;
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        if (savedInstanceState == null) {
+            mWebView = (WebView) findViewById(R.id.activity_main_webview);
+            // Activar Javascript
+            WebSettings webSettings = mWebView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            mWebView.setWebChromeClient(new WebChromeClient() {
+                public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                    callback.invoke(origin, true, false);
+                }
+            });
+            //Página de respiraxixon
+            mWebView.loadUrl("http://respiraxixon.com/rxp/");
+        } else {
+            ((WebView) findViewById(R.id.activity_main_webview)).restoreState(savedInstanceState);
+        }
+
+/*
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+*/
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState )
+    {
+        super.onSaveInstanceState(outState);
+        mWebView.saveState(outState);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+}
